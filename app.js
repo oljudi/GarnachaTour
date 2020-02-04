86eb6b5358e28648746d4a23d00a6179a81a81cb
Charly <cgtright@gmail.com>
2020-02-04 13:41:46 -0600

    pruebas

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
 
-
+"hola"
 mongoose
   .connect('mongodb://localhost/garnachatour', {useNewUrlParser: true})
   .then(x => {
